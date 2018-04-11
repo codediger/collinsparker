7677ec8f9c5cfaa96f8c21a47285b484bb8090df
codediger <orinamiolatunji@gmail.com>
2018-04-11 22:26:51 +0100

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import { GraphQLServer } from 'graphql-yoga'
+import { Prisma } from './generated/prisma'
+import { resolvers, fragmentReplacements } from './resolvers'
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      fragmentReplacements,
+      endpoint: process.env.PRISMA_ENDPOINT,
+      secret: process.env.PRISMA_SECRET,
+      debug: true,
+    }),
+  }),
+})
+
+server.start(({ port }) => console.log(`Server is running on http://localhost:${port}`))
---
+++ server/src/resolvers/Query.ts
@@ -0,0 +1,44 @@
+import { Context } from '../utils'
+
+export const Query = {
+  viewer: () => ({}),
+
+  topExperiences: async (parent, args, ctx: Context, info) => {
+    return ctx.db.query.experiences({ orderBy: 'popularity_DESC' }, info)
+  },
+
+  topHomes: async (parent, args, ctx: Context, info) => {
+    return ctx.db.query.places({ orderBy: 'popularity_DESC' }, info)
+  },
+
+  homesInPriceRange: async (parent, args, ctx: Context, info) => {
+    const where = {AND: [{pricing:{perNight_gte: args.min}}, {pricing:{perNight_lte: args.max}}]}
+    return ctx.db.query.places({ where }, info)
+  },
+
+  topReservations: async (parent, args, ctx: Context, info) => {
+    return ctx.db.query.restaurants({ orderBy: 'popularity_DESC' }, info)
+  },
+
+  featuredDestinations: async (parent, args, ctx: Context, info) => {
+    return ctx.db.query.neighbourhoods(
+      { orderBy: 'popularity_DESC', where: { featured: true } },
+      info,
+    )
+  },
+
+  experiencesByCity: async (parent, { cities }, ctx: Context, info) => {
+    return ctx.db.query.cities({
+      where: {
+        name_in: cities,
+        neighbourhoods_every: {
+          locations_every: {
+            experience: {
+              id_gt: '0',
+            },
+          },
+        },
+      },
+    })
+  },
+}
